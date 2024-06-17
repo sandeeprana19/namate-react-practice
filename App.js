d914ceee62bdc1bfd67b0d32cb979eb0b3b6439f
sandeep-rana-19 <96654527+sandeeprana191@users.noreply.github.com>
2024-06-18 01:27:01 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,29 @@
+// <div id="parent">
+//   <div id="child">
+//     <h1>I'm h1 tag</h1>
+//     <h2>I'm h2 tag</h2>
+//   </div>
+//   <div id="child2">
+//     <h1>I'm h1 tag</h1>
+//     <h2>I'm h2 tag</h2>
+//   </div>
+// </div>
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("header"));
+
+console.log(root);
+
+root.render(parent);
